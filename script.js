43af0f617a9292066c656a2a3ffaf808c172ee31
Tootlos <130791203+tootlos@users.noreply.github.com>
2025-06-21 11:03:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const input = document.querySelector(".search__input");
+const button = document.querySelector(".search__button");
+const select = document.querySelector(".search__select");
+
+button.addEventListener("click", submitRequest);
+
+function submitRequest(event) {
+    event.preventDefault();
+
+    const value = input.value;
+    const searchEngine = select.value;
+
+    let params;
+
+    if (searchEngine === "google" || searchEngine === "bing") {
+        params = "/search?q=";
+    } else if (searchEngine === "yandex") {
+        params = "/search?text=";
+    } else if (searchEngine === "duckduckgo") {
+        params = "?q="
+    }
+
+
+    window.location.href = `https://www.${searchEngine}.com/${params}` + value;
+
+
+}
